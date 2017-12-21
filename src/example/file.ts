ef8a395d478c4b2d418b7c5ad7e5b899cd57488d
Zongmin Lei <leizongmin@gmail.com>
2017-12-21 19:58:31 +0800

    add example html files

--- src/example/file.ts
+++ src/example/file.ts
@@ -1,22 +1,38 @@
 import * as path from "path";
 import * as fs from "fs";
 import { parse, toString } from "../lib";
+import * as colors from 'colors';
 
-fs.readFile(path.resolve(__dirname, "../../test/file.html"), (err, ret) => {
-  if (err) throw err;
-  const input = ret.toString();
+const file1 = fs
+  .readFileSync(path.resolve(__dirname, "../../test/file1.html"))
+  .toString();
+const file2 = fs
+  .readFileSync(path.resolve(__dirname, "../../test/file2.html"))
+  .toString();
+
+function parseHTML(content: string) {
+  const input = content.toString();
   const start = process.uptime();
   const { errors, nodes } = parse(input);
   const end = process.uptime();
   const html = toString(nodes);
   const end2 = process.uptime();
   if (errors.length > 0) {
-    console.error(errors);
+    for (const err of errors) {
+      console.log("[offset:%s]\t%s", err.position, colors.red(err.message));
+    }
   }
-  console.log(html);
+  // console.log(html);
+  const spent1 = end - start;
+  const spent2 = end2 - end;
+  const speed = html.length / spent1 / 1000000;
   console.log(
-    "parse: %ss, toString: %ss",
-    (end - start).toFixed(3),
-    (end2 - end).toFixed(3)
+    "parse: %ss (speed: %sM/s), toString: %ss",
+    spent1.toFixed(3),
+    speed.toFixed(1),
+    spent2.toFixed(3)
   );
-});
+}
+
+parseHTML(file1);
+parseHTML(file2);
