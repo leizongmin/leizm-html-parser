cbfd9711b4d0850a8683d996fe0bc634fa79ece8
老雷 <leizongmin@gmail.com>
2021-09-15 12:13:40 +0000

    docs: add example hello_deno.ts

---
+++ src/example/hello_deno.ts
@@ -0,0 +1,18 @@
+import html from "../../deps.ts";
+
+const { errors, nodes } = html.parse(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Hello, world! <small>by @leizm/html-parser</small></h1>
+  <p>Fast HTML parser written in pure JavaScript</p>
+</body>
+</html>`);
+
+console.log({ errors, nodes });
+console.log(html.toString(nodes));
